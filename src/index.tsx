6167632e4108bd2c93f89b7e981c8b09fa6ce2a6
Sergio Lazarkov <97618337+sergio1811x@users.noreply.github.com>
2023-01-13 13:26:05 +0300

    pizza

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,17 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
-
-import { BrowserRouter } from 'react-router-dom';
-import { store } from '../src/redux/store';
+import store from './redux/store';
 import { Provider } from 'react-redux';
-import * as React from 'react';
 
-const rootElem = document.getElementById('root');
-if (rootElem) {
-  const root = ReactDOM.createRoot(rootElem);
-  root.render(
-    <BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>,
-  );
-}
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
